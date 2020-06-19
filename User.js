90b84440d01b81467f3abb1315b8ad341bdfa952
Paul Gregor Musa <mpaulgregor@gmail.com>
2020-06-19 20:41:22 +0800

    test

---
+++ User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+	fullname: {
+		type: String,
+		required: true
+	},
+
+	username: {
+		type: String,
+		required: true
+	},
+	
+	password: {
+		type: String,
+		required: true
+	}
+});
+
+module.exports = mongoose.model("User", UserSchema);
